22f6d4c6f7c52860a5fd775e4a795f5ef9fb2789
Mahmmuod <mahmmuod.b2010@gmail.com>
2020-09-19 16:39:43 +0300

    Add project files.

---
+++ TwetterProject/Installers/MvcInstaller.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwetterProject.Installers
+{
+    public class MvcInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Tweetbook API",
+                    Description = "A simple example ASP.NET Core Web API"
+
+                });
+            });
+        }
+    }
+}
